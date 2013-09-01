6e3d73a2e135d26b097b0b15a7fa0bc7d1420f0f
维尼小弟 <jet.j.j.cheng@gmail.com>
2013-09-01 13:06:21 +0800

    init project

---
+++ src/main/java/com/wini/hadoopchina/HadoopchinaServletListener.java
@@ -0,0 +1,25 @@
+package com.wini.hadoopchina;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.http.HttpSessionEvent;
+
+import org.b3log.latke.servlet.AbstractServletListener;
+
+public class HadoopchinaServletListener extends AbstractServletListener {
+
+	@Override
+	public void requestInitialized(ServletRequestEvent servletRequestEvent) {
+		
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+		
+	}
+
+}
